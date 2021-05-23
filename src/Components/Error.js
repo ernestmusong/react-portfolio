9aa917dd951aeffa692512c67dfcae2022ecc735
Musong Ernest <ernestakeh@gmail.com>
2021-05-23 16:05:40 +0100

    added content to the error page

--- src/Components/Error.js
+++ src/Components/Error.js
@@ -2,9 +2,15 @@ import React from 'react'
 
 function Error() {
     return (
-        <>
-        <h1>hello from error page</h1>
-        </>
+        <div className="container h-100" style={{height:"100vh"}}>
+            <div className="row mx-auto h-100 w-100" style={{height:"100vh"}}>
+                <div className="col-12 d-flex align-items-center justify-content-center"style={{height:"100vh"}}>
+                     <h1 className="text-align-center text-capitalize text-bold">
+                         page not found!
+                     </h1>
+                </div>
+            </div>
+        </div>
     )
 }
 
